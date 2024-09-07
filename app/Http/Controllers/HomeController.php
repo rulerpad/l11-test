aee1cfc233a70bd69fb9c3a73993e8a6afccd906
maksimovich.gleb <maksimovich.gleb.dev@gmail.com>
2024-09-07 13:33:45 +0200

    app configuration; user count 100

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,16 +24,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = User::create([
+        $count = 100;
+
+        for ($index = 0; $index < $count; $index++) {
+            $this->createUser();
+        }
+
+        echo time();
+
+//        return view('home');
+    }
+
+    private function createUser(): void
+    {
+        User::create([
             'name' => time(),
             'email' => time() . '@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => time(),
         ]);
-
-        echo $user->id;
-
-//        return view('home');
     }
 }
